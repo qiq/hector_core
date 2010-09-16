16c086d4e0998cbeedb1f7fbdb911c981905f13a
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2010-09-16 12:53:40 +0000

    Removing obsolete code.

--- test/common.sh
+++ test/common.sh
@@ -29,9 +29,6 @@ function test_finish {
 	if [ -L test.log.props ]; then
 		rm test.log.props;
 	fi
-#	if [ $0 != '-bash' ]; then
-#		exit 1;
-#	fi
 }
 
 function test_server_start {
@@ -45,21 +42,6 @@ function test_server_start {
 
 
 }
-
-#function server_start {
-#        if ln -s $base/test/test.log.props $base/test.log.props 2>/dev/null; then
-#                clean=1
-#        fi
-#	if [ "$USE_VALGRIND" == 1 ]; then
-#		( cd $base; libtool --mode=execute valgrind --tool=memcheck --track-origins=yes --leak-check=full --leak-resolution=high --num-callers=20 --trace-children=yes --log-file=$base/test/${id}.log.valgrind hector_server -c $base/test/${id}-config.xml -f test & )
-#		client_wait_dontfail robot_processing_engine.run 0
-#	else
-#		if ! ( cd $base; hector_server -c ${base}/test/${id}-config.xml test ); then
-#			cat test.log 2>/dev/null
-#			doexit
-#		fi
-#	fi
-#}
 
 function test_compare_result {
 	diff -u $base/test/$id.log.correct $id.log.test
