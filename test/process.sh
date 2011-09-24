9f4b778512f668803fc3d0213a026f03824b16fc
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-09-24 13:54:12 +0000

    Waiting for valgrinded server to finish.

--- test/process.sh
+++ test/process.sh
@@ -6,11 +6,13 @@ id=process
 rm -f $id.out
 ln -s "$test_base/test/$id.data.in" . 2>/dev/null
 test_server_start $id test
+pid=$!
 hector_client_set PE_test.run 1
 hector_client -c "PROCESS PE_test" $HECTOR_HOST <$id.data.in >$id.data.out
 hector_client_wait M_simple[0].items 1000
 hector_client_set PE_test.run 0
 test_server_shutdown
+wait $!
 
 md5sum <$id.data.out >$id.log.result
 if [ -L $id.data.in ]; then
